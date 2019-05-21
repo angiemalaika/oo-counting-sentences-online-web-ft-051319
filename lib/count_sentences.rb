548dd9ed0bb959473cb307b6b1347338f3781ea1
Angela Mumba <malaika.mumba@gmail.com>
2019-05-21 17:47:48 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
+    # binding.pry 
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+  self.end_with?("!")
   end
 
   def count_sentences
+   self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.count
+   #/ \. | \? | \! /
+   
+#   / then question mark \? then "or" | then period \. then "or" | then exclamation point ! then / to end the expression.
 
+# /\?|\.|!/
   end
 end
